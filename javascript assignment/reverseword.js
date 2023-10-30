63c2f359ef4d5b34752a7336b40d3e52a946f6f8
amit828 <72549963+amit828@users.noreply.github.com>
2023-10-30 07:08:44 +0530

    Delete javascript assignment directory

--- javascript assignment/reverseword.js
+++
@@ -1,5 +0,0 @@
-const prompt = require("prompt-sync")({sigint:true});
-let str=prompt("please provide the string  ");
-let str1=str.split("").reverse().join("");
-let str2= str1.split(" ").reverse().join(" ");
-console.log(str2);
